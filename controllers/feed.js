4d537472f5cb20e632ce8b9f82387d21a9f2753b
sunilkumarhs <sunilkumarhs974117@gmail.com>
2024-05-21 21:11:37 +0530

    adding validation on server

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,16 +1,40 @@
+const { validationResult } = require("express-validator");
+
 exports.getPosts = (req, res, next) => {
   res.status(200).json({
-    posts: [{ title: "My First Post", content: "This is first post posted!" }],
+    posts: [
+      {
+        _id: "1",
+        title: "My First Post",
+        content: "This is first post posted!",
+        imageUrl: "./images/wallpaper.jpg",
+        creator: {
+          name: "sunil",
+        },
+        createdAt: new Date(),
+      },
+    ],
   });
 };
 
 exports.createPost = (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(422).json({
+      message: "Validation of Post is failed!!, entered data is incorrect!!",
+      errors: errors.array(),
+    });
+  }
   const title = req.body.title;
   const content = req.body.content;
-  console.log(title);
-  console.log(content);
   res.status(201).json({
     message: "Post created successfully!",
-    post: { id: new Date().toISOString(), title: title, content: content },
+    post: {
+      _id: new Date().toISOString(),
+      title: title,
+      content: content,
+      creator: { name: "sunil" },
+      createdAt: new Date(),
+    },
   });
 };
--- routes/feed.js
+++ routes/feed.js
@@ -1,9 +1,17 @@
 const express = require("express");
+const { body } = require("express-validator");
 const feedBackController = require("../controllers/feed");
 
 const router = express.Router();
 
 router.get("/posts", feedBackController.getPosts);
-router.post("/posts", feedBackController.createPost);
+router.post(
+  "/post",
+  [
+    body("title").trim().isLength({ min: 7 }),
+    body("content").trim().isLength({ min: 5 }),
+  ],
+  feedBackController.createPost
+);
 
 module.exports = router;
